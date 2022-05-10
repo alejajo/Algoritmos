a812e7709b537199aaeced10e2cdbb8d654b8972
alejajo <103969184+alejajo@users.noreply.github.com>
2022-05-10 16:20:54 -0600

    Algoritmo para sumar cierta cantidad de numeros

---
+++ Brute Force/sum_of_first.py
@@ -0,0 +1,12 @@
+def sum_of_firts():
+    n= int(input("Ingrese el numero hasta donde desea llegar:"))
+    print("n:",n)
+
+    suma=0
+
+    for num in range(n+1):
+
+        suma=suma+num
+
+    print("suma", suma)
+
